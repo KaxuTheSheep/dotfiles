bd28a94d62512782e6779b4fa440a27654a0455e
KaxuTheSheep <candyez699@gmail.com>
2026-03-04 10:47:44 +0000

    chore: remove wofi leftovers from v1

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,11 @@
-set -gx PATH $PATH $HOME/.cargo/bin
+set -U fish_greeting ""
+set -gx PATH $PATH $HOME/.cargo/bin $HOME/.local/bin
 set -gx STARSHIP_CONFIG $HOME/.config/starship/starship.toml
+
+if not pgrep -u $USER ssh-agent > /dev/null
+    eval (ssh-agent -c) > /dev/null
+end
+ssh-add ~/.ssh/id_dotfiles 2>/dev/null
+ssh-add ~/.ssh/id_obsidian 2>/dev/null
+
 starship init fish | source
-set -gx PATH $PATH $HOME/.local/bin
