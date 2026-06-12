ea3b21a089c6f80db9ccc4e003c86048a33a17a3
KaxuTheSheep <candyez699@gmail.com>
2026-06-12 22:04:46 +0000

    Feat: add niri config and fish config

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,69 @@
+set -U fish_greeting ""
+set -gx PATH $PATH $HOME/.cargo/bin $HOME/.local/bin
+set -gx STARSHIP_CONFIG $HOME/.config/starship/starship.toml
+
+# SSH agent
+if not pgrep -u $USER ssh-agent > /dev/null
+    eval (ssh-agent -c) > /dev/null
+end
+ssh-add ~/.ssh/id_linux 2>/dev/null
+
+# Aliases
+alias ls="eza"
+alias cat="bat"
+alias ff="fastfetch"
+alias ll="eza -lah --git"
+alias tree="eza --tree --all"
+
+# Functions
+function fdf
+    fd $argv | fzf
+end
+
+function cdf
+    cd (fd -t d --hidden | fzf $argv)
+end
+
+function vf
+    set file (fd -t f . --hidden --exclude node_modules --exclude .git | fzf --query "$argv" --exact --height 40% --reverse)
+    if test -n "$file"
+        nvim $file
+    end
+end
+
+function uvf
+    set file (fd -t f . / --hidden --exclude node_modules --exclude .git 2>/dev/null | fzf --query "$argv" --exact --height 40% --reverse)
+    if test -n "$file"
+        nvim $file
+    end
+end
+
+function cf
+    set file (fd -t f . --hidden --exclude node_modules --exclude .git | fzf --query "$argv" --exact --height 40% --reverse)
+    if test -n "$file"
+        bat $file
+    end
+end
+
+function ucf
+    set file (fd -t f . / --hidden --exclude node_modules --exclude .git 2>/dev/null | fzf --query "$argv" --exact --height 40% --reverse)
+    if test -n "$file"
+        bat $file
+    end
+end
+
+# Zoxide
+zoxide init fish | source
+
+# Starship
+starship init fish | source
+
+# Start niri if on TTY1 and not already running
+if status is-login
+    and test (tty) = /dev/tty1
+    and not pgrep -u $USER niri > /dev/null
+    exec niri-session
+end
+
+# Fastfetch on interactive login
+fastfetch
